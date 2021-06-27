edeca732d339728845f7ab199a5c12c27b98b0ab
Tara Rhoseyn <taragulwell@gmail.com>
2021-06-27 20:37:55 +0000

    Add array of objects to create the cards

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Card array for Easy mode. Credit for object array format: Fakorede Damilola
+let fruitCardsEasy = [
+    { name: "banana", img: "../images/fruit-one-banana.png", },
+    { name: "banana", img: "../images/fruit-one-banana.png", },
+    { name: "orange", img: "../images/fruit-two-orange.png", },
+    { name: "orange", img: "../images/fruit-two-orange.png", },
+    { name: "watermelon", img: "../images/fruit-three-watermelon.png", },
+    { name: "watermelon", img: "../images/fruit-three-watermelon.png", },
+    { name: "avocado", img: "../images/fruit-four-avocado.png", },
+    { name: "avocado", img: "../images/fruit-four-avocado.png", },
+    { name: "bluberry", img: "../images/fruit-five-blueberry.png", },
+    { name: "bluberry", img: "../images/fruit-five-blueberry.png", },
+    { name: "lime", img: "../images/fruit-six-lime.png", },
+    { name: "lime", img: "../images/fruit-six-lime.png", },
+];
