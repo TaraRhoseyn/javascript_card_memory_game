e0514612a5d2ff41a5d09b9ff2964d5cd391a586
Tara Rhoseyn <taragulwell@gmail.com>
2021-06-30 13:27:00 +0000

    add function to hide game board until game difficulty is selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,11 @@
 // @ts-check
 
-// Global variables
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+// Hides game until game difficulty level is selected
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('game').classList.add('no-display');
 });
 
-// Hide game element until level difficulty selected
-function hideGame() {
-    document.getElementById('game').classList.add('no-display');
-}
-
-if (document.readyState === "complete") {
-    // fired, run logic
-    hideGame();
-} else {
-    // not yet fired, add eventlistener
-    window.addEventListener("DOMContentLoaded", hideGame);
-}
-
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     console.log("DOM fully loaded and parsed");
-//     document.getElementById('game').classList.add('no-display');
-// });
-
-
+// Global variables
 let gameBoard = document.getElementsByClassName('.gameBoard');
 let fruitCards = document.getElementsByClassName('fruit-card');
 
@@ -38,22 +18,26 @@ document.getElementById('hard-button').addEventListener("click", startHardGame);
 function removeIntro() {
     document.getElementById("introduction-section").remove();
     document.getElementById("start-game-prompt").remove();
-    // document.getElementById('game').classList.add('yes-display');
 };
 
+function displayGame() {
+    document.getElementById('game').classList.add('yes-display');
+};
 
 // Functions that trigger the game depending on difficulty
 function startEasyGame() {
     removeIntro();
-    hideGame();
+    displayGame();
 };
 
 function startModerateGame() {
     removeIntro();
+    displayGame();
 };
 
 function startHardGame() {
     removeIntro();
+    displayGame();
 };
 
 
