11facbbec8181f23bc802f6322e520f4c7c2a1ab
Tara Rhoseyn <taragulwell@gmail.com>
2021-07-23 22:00:10 +0000

    test: Test email.js function

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,84 +1,98 @@
-// function initEmailJS() {
-//     emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
-// };
-// initEmailJS();
+// // function initEmailJS() {
+// //     emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
+// // };
+// // initEmailJS();
 
-// function validateMessage() {
-//     let name = document.getElementById("fromname");
-//     let email = document.getElementById("email");
-//     let message = document.getElementById("message");
-//     let submitButton = document.getElementById("submit-button")
+// const { EmailJSResponseStatus } = require("emailjs-com");
 
-//     submitButton.addEventListener("click", (e) => {
-//         e.preventDefault;
-//         if (name.value == "" || email.value == "" || message.value == "") {
-//             error();
-//         } else {
-//             sendEmail(name.value, email.value, message.value);
-//             success();
-//         }
-//     });
+// // function validateMessage() {
+// //     let name = document.getElementById("fromname");
+// //     let email = document.getElementById("email");
+// //     let message = document.getElementById("message");
+// //     let submitButton = document.getElementById("submit-button")
+
+// //     submitButton.addEventListener("click", (e) => {
+// //         e.preventDefault;
+// //         if (name.value == "" || email.value == "" || message.value == "") {
+// //             error();
+// //         } else {
+// //             sendEmail(name.value, email.value, message.value);
+// //             success();
+// //         }
+// //     });
+// // }
+// // validateMessage();
+
+// // function sendEmail(name, email, message) {
+// //     emailjs.send("gmail", "template_wod5ktu", {
+// //         to_name: "Card Memory Game Team",
+// //         from_name: name,
+// //         from_email: email,
+// //         message: message,
+// //     });
+// // }
+
+// // function error() {
+// //     console.log("Fix this later")
+// // }
+
+// // function success() {
+// //     console.log("Email sent")
+// // }
+
+// /**
+//  * [sendEmail to send email using EmailJS
+//  * Credit: Code Institute material "Sending Emails Using EmailJS"]
+//  * The API that is used is described here: https://www.emailjs.com/
+// //  */
+
+// //  function sendEmail(contactForm) {
+// //     emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
+// //     emailjs.send("service_ih1qkca", "template_wod5ktu", {
+// //         "from_name": contactForm.fromname.value,
+// //         "from_email": contactForm.email.value,
+// //         "message": contactForm.message.value
+// //     }).then(
+// //         function (response) {
+// //             // Success sending email
+// //         },
+// //         function (error) {
+// //             console.log("FAILED", error);
+// //         }
+// //     );
+// //     return false;  // To block from loading a new page
+// // }
+
+// function sendEmail(fullname, email, message) {
+//    emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
+//    let fullname = document.getElementById(fromname);
+//    let email = document.getElementById(email)
+//    let message = document.getElementById(message)
+//    emailjs.send("service_ih1qkca", "template_wod5ktu", {
+//        "from_name": fullname.value,
+//        "from_email": email.value,
+//        "message": message.value
+//    }).then(
+//        function (response) {
+//            // Success sending email
+//        },
+//        function (error) {
+//            console.log("FAILED", error);
+//        }
+//    );
+//    return false;  // To block from loading a new page
 // }
-// validateMessage();
-
-// function sendEmail(name, email, message) {
-//     emailjs.send("gmail", "template_wod5ktu", {
-//         to_name: "Card Memory Game Team",
-//         from_name: name,
-//         from_email: email,
-//         message: message,
-//     });
-// }
-
-// function error() {
-//     console.log("Fix this later")
-// }
-
-// function success() {
-//     console.log("Email sent")
-// }
-
-/**
- * [sendEmail to send email using EmailJS
- * Credit: Code Institute material "Sending Emails Using EmailJS"]
- * The API that is used is described here: https://www.emailjs.com/
-//  */
-
-//  function sendEmail(contactForm) {
-//     emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
-//     emailjs.send("service_ih1qkca", "template_wod5ktu", {
-//         "from_name": contactForm.fromname.value,
-//         "from_email": contactForm.email.value,
-//         "message": contactForm.message.value
-//     }).then(
-//         function (response) {
-//             // Success sending email
-//         },
-//         function (error) {
-//             console.log("FAILED", error);
-//         }
-//     );
-//     return false;  // To block from loading a new page
-// }
-
-function sendEmail(fullname, email, message) {
-   emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
-   let fullname = document.getElementById(fromname);
-   let email = document.getElementById(email)
-   let message = document.getElementById(message)
-   emailjs.send("service_ih1qkca", "template_wod5ktu", {
-       "from_name": fullname.value,
-       "from_email": email.value,
-       "message": message.value
-   }).then(
-       function (response) {
-           // Success sending email
-       },
-       function (error) {
-           console.log("FAILED", error);
-       }
-   );
-   return false;  // To block from loading a new page
-}
 
 
+function sendMail(params) {
+    var tempParams = {
+        from_name:document.getElementById('fromName').value,
+        to_name:document.getElementById('toName').value,
+        message:document.getElementById('msg').value,
+    };
+
+    emailjs.send('service_ih1qkca', 'template_wod5ktu', tempParams)
+    .then(function(res) {
+        console.log("sucess", res.status);
+    })
+};
