f1550fc529f328385e83f71a39207a3690fee905
TaraRhoseyn <taragulwell@gmail.com>
2021-07-26 14:55:22 +0000

    fix: Fix inconsistent syntax in email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,13 +1,13 @@
-document.getElementById('message-button').addEventListener("click", sendEmail);
+document.getElementById('message-button').addEventListener('click', sendEmail);
 
 // Credit: Code Institue and Email JS
 // Bug fix: Use contactForm method to prevent form closing
 function sendEmail(contactForm) {
-    emailjs.init("user_cQ4AB0KpJse0GG5cv2LdL");
-    emailjs.send("service_ih1qkca", "template_wod5ktu", {
-       "from_name": contactForm.fromname.value,
-       "from_email": contactForm.email.value,
-       "message": contactForm.message.value
+    emailjs.init('user_cQ4AB0KpJse0GG5cv2LdL');
+    emailjs.send('service_ih1qkca', 'template_wod5ktu', {
+       'from_name': contactForm.fromname.value,
+       'from_email': contactForm.email.value,
+       'message': contactForm.message.value
     }).then(
         function (response) {
            alert('Your message has been sent!')
