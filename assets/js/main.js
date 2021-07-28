77709faedf9d81aa0c83214792e03f55df72ef6b
Tara Rhoseyn <taragulwell@gmail.com>
2021-07-28 20:02:07 +0000

    style: Add hover styles to nav and a elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,17 @@
 // Makes sure the 'active' indicator on navbar is there upon page loading 
 window.onload = setActive;
 
+// Global variable for nav elements
+const nav = document.getElementById('nav').getElementsByTagName('a');
+
 /* Function takes navbar elements and checks if their href
- matches to the document.location, letting users know what
+matches to the document.location, letting users know what
 page they're on.
 Credit for function: CSS Deck. https://cssdeck.com/blog/intelligent-navigation/ */
 function setActive() {
-    var nav = document.getElementById('nav').getElementsByTagName('a');
     for(i = 0; i < nav.length; i++) { 
         if(document.location.href.indexOf(nav[i].href)>=0) {
             nav[i].className='active';
         }
     }
-}
+};
